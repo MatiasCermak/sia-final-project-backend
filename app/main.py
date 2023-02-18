8a8fdc31c791a87a72521442366b645ba8570d1a
Mcermak <matias.cermak@gmail.com>
2023-02-18 01:38:21 -0300

    Added cors policy  and movement to de processObstacle response

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.robot_api import RobotApi
 from model.response import Position
 from neuralnetwork.neural_network import NeuralNetwork
@@ -6,6 +7,13 @@ import uvicorn
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
@@ -31,6 +39,8 @@ async def processObstacle(request: Position):
         return response
     else:
         response = await robot_api.make_request("GET")
+        response.Resp.__setattr__("M1", nn_output.M1)
+        response.Resp.__setattr__("M2", nn_output.M2)
     return response
 
 if __name__ == "__main__":
