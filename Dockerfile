5c6f92fe4bb4efc086d187e45f8a0dc70513016f
Mcermak <matias.cermak@gmail.com>
2023-02-24 14:12:00 -0300

    Final changes, changed docker configuration

--- Dockerfile
+++ Dockerfile
@@ -20,4 +20,4 @@ COPY ./app /app
 RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
 USER appuser
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
+CMD ["python", "main.py"]
--- app/main.py
+++ app/main.py
@@ -4,10 +4,17 @@ from api.robot_api import RobotApi
 from model.response import Position, Response
 from neuralnetwork.neural_network import NeuralNetwork
 import uvicorn
-from utils.constants import TESTING_SET, INITIAL_WEIGHTS, TRAINING_SET
+from utils.constants import TESTING_SET, INITIAL_WEIGHTS, TRAINING_SET, LEARNING_RATE, BIAS, USE_BIAS, EPOCHS
 
 app = FastAPI()
-neural_network = NeuralNetwork(weights=INITIAL_WEIGHTS,learning_rate=[0.0076, 0.001], use_bias=[True, False], epochs=[10000, 500], bias=[0.76, 0.01])
+
+NEURAL_NETWORK_MODELS = {
+    'p-v': NeuralNetwork(weights=INITIAL_WEIGHTS, learning_rate=LEARNING_RATE, use_bias=USE_BIAS, epochs=EPOCHS, bias=BIAS, vectorizar=True),
+    'p-nv': NeuralNetwork(weights=INITIAL_WEIGHTS, learning_rate=LEARNING_RATE, use_bias=USE_BIAS, epochs=EPOCHS, bias=BIAS, vectorizar=False)
+}
+NN_MODEL = 'p-v'
+NEURAL_NETWORK = NEURAL_NETWORK_MODELS.get(NN_MODEL)
+NEURAL_NETWORK_TESTS = []
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,54 +24,102 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def root():
-    return {"message": "Hello World"} 
+    return {"message": "Hello World"}
+
 
 @app.get("/status")
 def status():
-    return 
+    return
+
 
 @app.get("/getObstacle")
 async def getObstacle():
     robot_api = RobotApi()
-    response = await robot_api.make_request("GET")
+    response = robot_api.make_request("GET")
     return response
+
 
 @app.post("/processObstacle")
 async def processObstacle(request: Position):
     robot_api = RobotApi()
-    nn_output = neural_network.process(request=request)
+    nn_output = NEURAL_NETWORK.process(request=request)
     response = robot_api.make_request("POST", nn_output)
-    
-    if(response.Resp == "OK"):
+
+    if (response.Resp == "OK"):
         response = robot_api.make_request("GET")
         response.Resp.__setattr__("M1", nn_output.M1)
         response.Resp.__setattr__("M2", nn_output.M2)
-    response.__setattr__("NNResponse", nn_output);
+    response.__setattr__("NNResponse", nn_output)
     return response
 
+
 @app.get("/test")
-async def testNetwork():
+def testNetwork():
+    global NEURAL_NETWORK_TESTS
+    NEURAL_NETWORK_TESTS.clear()
     errors = 0
     robot_api = RobotApi()
     distinct_positions = []
     respuestas = ""
-    while len(distinct_positions) < 9 :
-        get_response = robot_api.make_request("GET")
-        if get_response.Resp in distinct_positions:
-            continue
-        distinct_positions.append(get_response.Resp)
-        nn_output = neural_network.process(request=get_response.Resp)
-        post_response = robot_api.make_request("POST", nn_output)
-        post_response.__setattr__("NNResponse", nn_output)
-        respuestas += "Resp: " + str(post_response.Resp) + "Desc: " + post_response.Desc + "NNResponse: " + str(post_response.NNResponse) + "\n"
-        if(post_response.Resp == "KO"):
-            errors += 1
-    post_response.__setattr__("Resp", "Testeo completo")
-    post_response.__setattr__("Desc", "Errores en testeo: "+str(errors))
-    post_response.__setattr__("NNResponse", respuestas)
-    return post_response
+    for id  in NEURAL_NETWORK_MODELS:
+        while len(distinct_positions) < 9:
+            get_response = robot_api.make_request("GET")
+            if get_response.Resp in distinct_positions:
+                continue
+            distinct_positions.append(get_response.Resp)
+            nn_output = NEURAL_NETWORK_MODELS.get(id).process(request=get_response.Resp)
+            post_response = robot_api.make_request("POST", nn_output)
+            post_response.__setattr__("NNResponse", nn_output)
+            respuestas += "Resp: " + str(post_response.Resp) + "Desc: " + \
+                post_response.Desc + "NNResponse: " + \
+                str(post_response.NNResponse) + "\n"
+            if (post_response.Resp == "KO"):
+                errors += 1
+        post_response.__setattr__("Resp", "Testeo completo - " + id)
+        post_response.__setattr__("Desc", "Errores en testeo: "+str(errors))
+        post_response.__setattr__("NNResponse", respuestas)
+        NEURAL_NETWORK_TESTS.append(post_response)
+    
+
+
+@app.get("/changeModel/{model}")
+async def changeModel(model: str):
+    global NEURAL_NETWORK
+    global NN_MODEL
+    if model == NN_MODEL:
+        return {'status': "OK",
+                "bias": BIAS,
+                "useBias": USE_BIAS,
+                "epochs": EPOCHS,
+                "initialWeights": INITIAL_WEIGHTS,
+                "learningRate": LEARNING_RATE,
+                "tests": NEURAL_NETWORK_TESTS}
+
+    nnetwork = NEURAL_NETWORK_MODELS.get(model)
+    if (nnetwork != None):
+        NEURAL_NETWORK = nnetwork
+        NN_MODEL = model
+        return {'status': "OK",
+                "bias": BIAS,
+                "useBias": USE_BIAS,
+                "epochs": EPOCHS,
+                "initialWeights": INITIAL_WEIGHTS,
+                "learningRate": LEARNING_RATE,
+                "tests": NEURAL_NETWORK_TESTS}
+
+    return {'status': "KO",
+            "bias": BIAS,
+            "useBias": USE_BIAS,
+            "epochs": EPOCHS,
+            "initialWeights": INITIAL_WEIGHTS,
+            "learningRate": LEARNING_RATE,
+                "tests": NEURAL_NETWORK_TESTS}
 
 if __name__ == "__main__":
+    print("Please wait, running tests...")
+    testNetwork()
     uvicorn.run(app, host="0.0.0.0", port=8080)
+
